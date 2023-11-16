551631c4e998d8e6ec8ed7d55027281933bbaed5
abomesto <eyas.adi.88@gmail.com>
2023-11-16 09:45:04 +0100

    Installed proptypes and corrected the login path

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -13,7 +13,7 @@ export const LoginView = ({ onLoggedIn }) => {
         Password: password
       };
 
-    fetch("https://iyas-movies-d1500c6f9580.herokuapp.com/movies", {
+    fetch("https://iyas-movies-d1500c6f9580.herokuapp.com/login", {
       method: "POST",
       headers: {
         'Content-Type': 'application/json',
